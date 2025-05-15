d39165e868f563f500a5a70fa47e4b13add10fe1
Madhava Jay <me@madhavajay.com>
2025-05-15 12:39:29 +1000

    Cleaning up template

--- fastsyftbox/syftbox.py
+++ fastsyftbox/syftbox.py
@@ -7,21 +7,22 @@ import asyncio
 
 
 class Syftbox:
-    def __init__(self, app: FastAPI, name: str = None, data_dir: str = "./data", config: SyftClientConfig = None):
-        name = name if name is not None else Path(__file__).resolve().parent.name
+    def __init__(self, app: FastAPI, name: str, data_dir: str = "./data", config: SyftClientConfig = None):
         self.name = name
         self.app = app
-        self.current_dir = Path(__file__).parent
-        self.data_dir = Path(data_dir)
 
         # Load config + client
         self.config = config if config is not None else SyftClientConfig.load()
         self.client = SyftboxClient(self.config)
 
+        # setup app data directory
+        self.current_dir = Path(__file__).parent
+        self.app_data_dir = Path(self.client.config.data_dir) / "private" / "app_data" /  name
+        self.app_data_dir.mkdir(parents=True, exist_ok=True)
+
         # Setup event system
         self.box = SyftEvents(app_name=name)
         self.client.makedirs(self.client.datasite_path / "public" / name)
-        self.data_dir.mkdir(parents=True, exist_ok=True)
 
         # Attach lifespan
         self._attach_lifespan()
